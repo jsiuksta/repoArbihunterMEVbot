e32b329bca1bae227ff7d40d38af9368d81e2043
JULIAN J SIUKSTA <jsiuksta@hotmail.com>
2025-08-10 21:38:29 +1000

    Add files via upload

---
+++ ArbHunterGitCopilotMEVBot.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+// Interfaces (Simplified, for brevity)
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
+}
+
+interface IAaveFlashLoan {
+    function flashLoan(
+        address receiver,
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata modes,
+        address onBehalfOf,
+        bytes calldata params,
+        uint16 referralCode
+    ) external;
+}
+
+interface IUniswapV2Router {
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function swapExactTokensForTokens(
+        uint amountIn, uint amountOutMin,
+        address[] calldata path, address to, uint deadline
+    ) external returns (uint[] memory amounts);
+}
+
+// Add other protocol interfaces as needed for Sushi, 1inch, etc.
+
+// CONFIG
+address constant OWNER = 0x551CD6C342B864359CfD4B0833C0ce8EeADC543C;
+address constant AAVE_POOL = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9;
+address constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+address constant ETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+address constant VMINT = 0xD7B2C1a7F3c67fB0EA57a7ef29bC1F18D7bE3195;
+address constant sUSDS = 0xa3931d71877c0e7a3148cb7eb4463524fec27fbd;
+address constant USDL = 0xbdC7c08592Ee4aa51D06C27Ee23D5087D65aDbcD;
+address constant TREAT = 0xa02c49da76a085e4e1ee60a6b920ddbc8db599f4;
+address constant SSLP = 0x413075439b544cb6f580dfa56272ed13fdd8d90e;
+address constant LEO = 0x2af5d2ad76741191d15dfe7bf6ac92d4bd912ca3;
+address constant BGB = 0x54D2252757e1672EEaD234D27B1270728fF90581;
+address constant SPX = 0xe0f63a424a4439cbe457d80e4f4b51ad25b2c56c;
+address constant VIRTUAL = 0x44ff8620b8cA30902395A7bD3F2407e1A091BF73;
+address constant UBEX = 0x6704b673c70de9bf74c8fba4b4bd748f0e2190e1;
+address constant BLOCK = 0xCaB84bc21F9092167fCFe0ea60f5CE053ab39a1E;
+address constant  PLSPAD = 0x8a74bc8c372bc7f0e9ca3f6ac0df51be15aec47a;
+address constant BUSY = 0x5CB3ce6D081fB00d5f6677d196f2d70010EA3f4a;
+address constant MANYU = 0x95AF4aF910c28E8EcE4512BFE46F1F33687424ce;
+address constant TrueGBP = 0x00000000441378008ea67f4284a57932b1c000a5;
+address constant GYEN = 0xC08512927D12348F6620a698105e1BAac6EcD911;
+address constant EURC=0x1abaea1f7c830bd89acc67ec4af516284b1bc33c;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Add all asset addresses...
+
+address constant UNISWAP_ROUTER = /* Uniswap v2 Router address */;
+// Add other protocol addresses...
+
+contract ArbHunterGitCopilotMEVBot {
+
+    // Asset list for arbitrage
+    address[] public assets = [
+        USDT, USDC, ETH, WETH, /* ... all other asset addresses ... */
+    ];
+
+    // Protocol routers
+    address[] public routers = [
+        UNISWAP_ROUTER, /* ... all other router addresses ... */
+    ];
+
+    // Entry point: Find and execute arbitrage via flash loan
+    function huntArb() external {
+        // Borrow 1,000 USDT via AAVE flash loan
+        address[] memory flashAssets = new address[](1);
+        flashAssets[0] = USDT;
+        uint256[] memory flashAmounts = new uint256[](1);
+        flashAmounts[0] = 1000e6; // USDT has 6 decimals
+        uint256[] memory modes = new uint256[](1);
+        modes[0] = 0; // 0 = no debt, flash loan
+
+        IAaveFlashLoan(AAVE_POOL).flashLoan(
+            address(this),
+            flashAssets,
+            flashAmounts,
+            modes,
+            address(this),
+            "", // params
+            0
+        );
+    }
+
+    // AAVE flash loan callback
+    function executeOperation(
+        address[] calldata assets,
+        uint256[] calldata amounts,
+        uint256[] calldata premiums,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool) {
+        // Example: find best opportunity between routers for USDT
+        uint256 bestProfit = 0;
+        address bestRouter;
+        address bestAssetOut;
+
+        // For demonstration: scan asset pairs on each router
+        for (uint i = 0; i < routers.length; i++) {
+            for (uint j = 0; j < assets.length; j++) {
+                if (assets[j] == USDT) continue;
+                // Simulate swap USDT -> asset -> USDT
+                address[] memory path = new address[](2);
+                path[0] = USDT;
+                path[1] = assets[j];
+                uint[] memory out1 = IUniswapV2Router(routers[i]).getAmountsOut(amounts[0], path);
+
+                path[0] = assets[j];
+                path[1] = USDT;
+                uint[] memory out2 = IUniswapV2Router(routers[i]).getAmountsOut(out1[1], path);
+
+                uint profit = out2[1] > amounts[0] ? out2[1] - amounts[0] : 0;
+                if (profit > bestProfit) {
+                    bestProfit = profit;
+                    bestRouter = routers[i];
+                    bestAssetOut = assets[j];
+                }
+            }
+        }
+
+        // Execute arbitrage if profitable
+        if (bestProfit > 0) {
+            IERC20(USDT).approve(bestRouter, amounts[0]);
+            // Swap USDT -> bestAssetOut
+            address[] memory path1 = new address[](2);
+            path1[0] = USDT;
+            path1[1] = bestAssetOut;
+            IUniswapV2Router(bestRouter).swapExactTokensForTokens(
+                amounts[0], 1, path1, address(this), block.timestamp
+            );
+
+            // Swap bestAssetOut -> USDT
+            IERC20(bestAssetOut).approve(bestRouter, IERC20(bestAssetOut).balanceOf(address(this)));
+            address[] memory path2 = new address[](2);
+            path2[0] = bestAssetOut;
+            path2[1] = USDT;
+            IUniswapV2Router(bestRouter).swapExactTokensForTokens(
+                IERC20(bestAssetOut).balanceOf(address(this)), 1, path2, address(this), block.timestamp
+            );
+        }
+
+        // Repay flash loan
+        uint256 totalDebt = amounts[0] + premiums[0];
+        IERC20(USDT).approve(AAVE_POOL, totalDebt);
+
+        // Send profit to OWNER
+        uint256 profitFinal = IERC20(USDT).balanceOf(address(this)) - totalDebt;
+        if (profitFinal > 0) {
+            IERC20(USDT).transfer(OWNER, profitFinal);
+        }
+
+        return true;
+    }
+
+    // Allow contract to receive ETH
+    receive() external payable {}
+}
